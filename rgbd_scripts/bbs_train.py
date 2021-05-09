9ececbe352931baeeb2152e3bdffdb8813279b30
ArtamonovDen <brightcolors46@gmail.com>
2021-05-09 13:49:41 +0300

    Add bracets

--- rgbd_scripts/bbs_train.py
+++ rgbd_scripts/bbs_train.py
@@ -144,7 +144,7 @@ def main(args_):
     wandb_config = init_wnb_config(args_)
 
     with wandb.init(project=wandb_config['wandb_project'], config=wandb_config):
-        # wandb.watch(model, loss, log="all")
+        wandb.watch(model(), loss, log="all")
         logging.info('Start training...')
 
         best_mae = 0.0
