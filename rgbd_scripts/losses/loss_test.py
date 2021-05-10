2ec760a3a56b239d0043287bff76546edae0be02
ArtamonovDen <brightcolors46@gmail.com>
2021-05-10 10:16:09 +0300

    Update dice loss

--- rgbd_scripts/losses/loss_test.py
+++ rgbd_scripts/losses/loss_test.py
@@ -1,11 +1,11 @@
 import unittest
 import torch
-from loss import loss
+from losses.dice_loss import DiceLoss
 
 
 class LossTest(unittest.TestCase):
     def setUp(self):
-        self.output_shape = (1, 1, 3, 3)
+        self.output_shape = (3, 1, 3, 3)
 
         self.target = self.make_batch(torch.tensor([
             [0, 1, 0],
@@ -19,15 +19,15 @@ class LossTest(unittest.TestCase):
         return torch.cat(self.output_shape[0] * [input]).reshape(self.output_shape)
 
     def test_dice_loss_with_gt_inut(self):
-
         gt_input = self.make_batch(torch.tensor([
             [0, 1, 0],
             [1, 1, 1],
             [0, 1, 0],
         ]))
-        dice_loss = loss.dice_loss(
-            gt_input, self.target, apply_sigmoid=False, smooth=1)
-        self.assertAlmostEqual(0, dice_loss.item())
+
+        dice_loss = DiceLoss(apply_sigmoid=False, smooth=1)
+        ret_loss = dice_loss(gt_input, self.target)
+        self.assertAlmostEqual(0, ret_loss.item())
 
     def test_dice_loss_with_wrong_input(self):
         fully_wrong_input = self.make_batch(torch.tensor([
@@ -35,9 +35,22 @@ class LossTest(unittest.TestCase):
             [0, 0, 0],
             [1, 0, 1],
         ]))
-        dice_loss = loss.dice_loss(
-            fully_wrong_input, self.target, apply_sigmoid=False, smooth=1)  # more the batch size, more loss: smooth is applied over a batch
-        self.assertAlmostEqual(0.9, dice_loss.item())
+
+        dice_loss = DiceLoss(apply_sigmoid=False, smooth=0.0)
+        ret_loss = dice_loss(fully_wrong_input, self.target)
+        self.assertAlmostEqual(1.0, ret_loss.item())
+
+    def test_dice_loss(self):
+        inp = self.make_batch(torch.tensor([
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+        ]))
+
+        dice_loss = DiceLoss(apply_sigmoid=False, smooth=0.0)
+        ret_loss = dice_loss(inp, self.target)
+        print(ret_loss)
+        self.assertAlmostEqual(0.25, ret_loss.item())
 
 
 if __name__ == '__main__':
