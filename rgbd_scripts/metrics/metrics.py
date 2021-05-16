d89eec3f27266c7729f0de77bf2167a2edb6a3e4
ArtamonovDen <brightcolors46@gmail.com>
2021-05-16 16:05:11 +0300

    Start building s-measure

--- rgbd_scripts/metrics/metrics.py
+++ rgbd_scripts/metrics/metrics.py
@@ -57,12 +57,38 @@ def f_beta_score_with_threshold(pred: np.ndarray, gt: np.ndarray, threshold_num=
     return result
 
 
-def s_measure():
+def _object(pred: np.ndarray, gt: np.ndarray):
+    temp = pred[gt == 1]
+    x = temp.mean()
+    sigma_x = temp.std()
+    score = 2.0 * x / (x * x + 1.0 + sigma_x + 1e-20)
+
+    return score
+
+
+def _s_object(pred: np.ndarray, gt: np.ndarray):
+    zero_pred = np.zeros_like(pred)
+    fg = np.where(gt == 0, zero_pred, pred)
+    bg = np.where(gt == 1, zero_pred, 1 - pred)
+    o_fg = _object(fg, gt)
+    o_bg = _object(bg, 1 - gt)
+    µ = gt.mean()  # Let µ be the ratio of foreground area in GT to imagearea (width ∗ height).
+    s_o = µ * o_fg + (1 - µ) * o_bg
+    return s_o
+
+
+def _s_region(pred: np.ndarray, gt: np.ndarray):
+    return 0
+
+
+def s_measure(pred: np.ndarray, gt: np.ndarray, alpha: float = 0.5):
     """
         Calculates Structure-measure: https://arxiv.org/abs/1708.00786
+        Implementaion inspired by https://github.com/zzhanghub/eval-co-sod
 
     """
-    pass
+    Q = alpha * _s_object(pred, gt) + (1 - alpha) * _s_region(pred, gt)
+    return Q
 
 
 def e_measure():
